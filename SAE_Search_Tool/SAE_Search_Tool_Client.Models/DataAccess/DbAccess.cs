366d3a5f75eb6d4f4dc00a87284e2c0d0d730d4e
Yotooof <58489578+Yotooof@users.noreply.github.com>
2021-06-23 19:42:47 +0200

    Update DBAccess

--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/DataAccess/DbAccess.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/DataAccess/DbAccess.cs
@@ -2,8 +2,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace SAE_Search_Tool_Client.Models.DataAccess
 {
@@ -12,5 +14,15 @@ namespace SAE_Search_Tool_Client.Models.DataAccess
     /// </summary>
     class DbAccess
     {
+        protected void SQLTest(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("data source=.;database=Sample; integraded security=SSPI"); // data source=. = local server
+            SqlCommand cmd = new SqlCommand("Select * from Yoink", con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            public DataSet ds = new DataSet();
+            con.Close();
+        }      
     }
 }
+
