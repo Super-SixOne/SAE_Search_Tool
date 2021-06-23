03a61f1592df863563c9581072c11db07c76ec9f
Super-SixOne <dominik.goeggerle@its-stuttgart.de>
2021-06-23 18:11:43 +0200

    Folder structure for models project

---
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/DataAccess/DbAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_Search_Tool_Client.Models.DataAccess
+{
+    class DbAccess
+    {
+    }
+}
