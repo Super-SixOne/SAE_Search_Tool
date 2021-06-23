c8bfad22f37edd4b2b55b58205fe9c069da01f6a
Super-SixOne <dominik.goeggerle@its-stuttgart.de>
2021-06-23 18:36:15 +0200

    Steven hat geduscht

--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/DataAccess/DbAccess.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/DataAccess/DbAccess.cs
@@ -9,7 +9,6 @@ namespace SAE_Search_Tool_Client.Models.DataAccess
     /// <summary>
     /// Class that provides functionality to access a database.
     /// </summary>
-    /// Steven stinkt, push test
     class DbAccess
     {
     }
