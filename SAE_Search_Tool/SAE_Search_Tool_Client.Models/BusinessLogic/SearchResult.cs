8b0e2e1705945de37abd77a40f87b91716d58ae3
Super-SixOne <dominik.goeggerle@its-stuttgart.de>
2021-06-23 18:45:44 +0200

    added SearchResult class

---
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/SearchResult.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAE_Search_Tool_Client.Models.BusinessLogic
+{
+    /// <summary>
+    /// Class that represents one search result of a query to the database.
+    /// </summary>
+    class SearchResult
+    {
+        #region Properties
+
+        string Path { get; set; }
+        string TextExcerpt { get; set; }
+
+        #endregion
+    }
+}
