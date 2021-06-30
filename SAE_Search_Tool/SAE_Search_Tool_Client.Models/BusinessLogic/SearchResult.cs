431084d475f672586ae6c8d5965b6cabe6b8f9c0
StevenITS <86018233+StevenITS@users.noreply.github.com>
2021-06-30 17:36:56 +0200

    Started list Attempt Json

--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/SearchResult.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/SearchResult.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace SAE_Search_Tool_Client.Models.BusinessLogic
 {
@@ -17,5 +18,31 @@ namespace SAE_Search_Tool_Client.Models.BusinessLogic
         public string TextExcerpt { get; set; }
 
         #endregion
+    }
+    public class JsonCreation
+    {
+        //list
+        //_List creation
+        
+       public List<data> _DriveList = new List<data>();
+       _Dr
+        
+        _DriveList.Add(new data()
+        {
+            Drive = "C:",
+            SubFolders = @"DataSubfolder\Kacke\Dumm"
+        });
+           
+
+          
+
+
+        string json = JsonConvert.SerializeObject(_list.toArray());
+
+    }
+    public class data
+    {
+     public string Drive { get; set; }
+     public string SubFolders { get; set; }
     }
 }
