8f62341446c1b3f84578f03615488808ef5a7d36
StevenITS <86018233+StevenITS@users.noreply.github.com>
2021-07-09 08:33:11 +0200

    restructure classes

--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/DatabaseAccess.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/DatabaseAccess.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace SAE_Search_Tool_Client.Models.BusinessLogic
 {
-    class DatabaseAccess
-    {
+
+        public class DataFromDB
+        {
+            public long IdData { get; set; }
+
+            private string _Path;
+            public string Path
+            {
+                get
+                {
+                    return _Path;
+                }
+
+                set
+                {
+                    _Path = value;
+                }
+            }
+            public string EingabeText { get; set; }
+
+        }
     }
-}
+
--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/JsonCreation.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/JsonCreation.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SAE_Search_Tool_Client.Models.BusinessLogic
-{
-    class Class1
     {
+        public class JsonCreation
+        {
+            //list
+            //_List creation
+
+            List<Data> _DriveList = new List<Data>();
+            public void InitializeTestData()
+            {
+                _DriveList.Add(new Data()
+                {
+                    Drive = "C:",
+                    SubFolders = @"DataSubfolder\Kacke\Dumm"
+                });
+                string json = JsonConvert.SerializeObject(_DriveList);
+                File.WriteAllText(Path.GetFullPath, json);
+            }
+
+        }
+        public class Data
+        {
+            public string Drive { get; set; }
+            public string SubFolders { get; set; }
+        }
     }
 }
