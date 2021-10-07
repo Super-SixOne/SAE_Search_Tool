a13b83f845ec9579efa2fc62393de13babe53205
Super-SixOne <dominik.goeggerle@its-stuttgart.de>
2021-10-07 17:09:42 +0200

    cleanup

--- SAE_Search_Tool/SAE_Search_Tool_Sync_Service/Logic/FileReader.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Sync_Service/Logic/FileReader.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SAE_Search_Tool_Sync_Service.Logic.Models;
+﻿using SAE_Search_Tool_Sync_Service.Logic.Models;
 
 namespace SAE_Search_Tool_Sync_Service.Logic
 {
