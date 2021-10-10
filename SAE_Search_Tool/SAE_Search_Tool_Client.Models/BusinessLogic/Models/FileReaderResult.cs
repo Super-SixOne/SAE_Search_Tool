cc25454a67e6f0b160df60f63c5384e368028612
Super-SixOne <86012996+Super-SixOne@users.noreply.github.com>
2021-10-10 15:06:10 +0200

    implemented Hashing method

--- SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/Models/FileReaderResult.cs
+++ SAE_Search_Tool/SAE_Search_Tool_Client.Models/BusinessLogic/Models/FileReaderResult.cs
@@ -1,4 +1,8 @@
-﻿namespace SAE_Search_Tool_Client.Models.BusinessLogic.Models
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SAE_Search_Tool_Client.Models.BusinessLogic.Models
 {
     public class FileReaderResult
     {
@@ -6,7 +10,7 @@
         {
             this.Path = path;
             this.Content = content;
-            this.SHA512 = GetHash();
+            this.SHA512 = GetSHA512Base64();
         }
 
         public FileReaderResult(string path, string content, string sha512) : this(path, content)
@@ -18,10 +22,16 @@
         public string Content { get; set; }
         public string SHA512 { get; set; }
 
-        private string GetHash()
+        private string GetSHA512Base64()
         {
-            // TODO: Logic for hash
-            return "hashValue";
+            string base64Hash;
+
+            using(SHA512Managed shaM = new SHA512Managed())
+            {
+               base64Hash = Convert.ToBase64String(shaM.ComputeHash(Encoding.UTF8.GetBytes(this.Path + this.Content)));
+            }
+
+            return base64Hash;
         }
     }
 }
